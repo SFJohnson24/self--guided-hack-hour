7b5b56a920f2995bed1a4ca8a55368bc854405d3
SFJohnson24 <SFJohnson24@gmail.com>
2024-01-16 12:18:07 -0500

    tuesday problems

---
+++ 7 trees/bst-height.java
@@ -0,0 +1,8 @@
+package 7 trees;
+
+public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
